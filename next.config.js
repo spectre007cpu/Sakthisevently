6271ef1d9216a37a18af672cb18968dd92a2dab2
eventsakthi007 <23s3project2024@gmail.com>
2024-08-06 13:13:07 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['utfs.io'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'utfs.io',
-        port: ''
-      }
-    ]
-  }
-}
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
